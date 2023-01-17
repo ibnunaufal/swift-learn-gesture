949c8751c583b4bda0b1533da8fd63b33f870063
ibnu naufal <ibnunaufal5758@gmail.com>
2023-01-17 10:32:48 +0700

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Macbook Pro on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
